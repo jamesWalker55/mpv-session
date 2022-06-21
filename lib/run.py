eb6ba523cdf5dc05e88a5e30ffcca1b578ef73eb
James Walker <james.chunho@gmail.com>
2022-06-22 00:30:39 +0100

    Add run module

---
+++ lib/run.py
@@ -0,0 +1,21 @@
+import subprocess
+
+
+MPV_PATH = "mpv"
+
+
+def set_mpv_path(path):
+    global MPV_PATH
+    MPV_PATH = path
+
+
+def mpv(playlist_items, playlist_start, playback_pos):
+    args = [
+        MPV_PATH,
+        *playlist_items,
+        "--pause",
+        f"--playlist-start={playlist_start}",
+        f"--script-opts=initial-seek={playback_pos}",
+    ]
+
+    return subprocess.Popen(args, creationflags=subprocess.DETACHED_PROCESS)
